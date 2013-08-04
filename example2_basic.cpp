3ecc67ae97303bbb902d8db527d8ceed15e166aa
lordoffox <lordoffox@gmail.com>
2013-08-04 11:49:42 +0800

    Create example2_basic.cpp

---
+++ example2_basic.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "ajson.hpp"
+
+using namespace std;
+using namespace boost::ajson;
+
+struct Education
+{
+  string	School;
+	double	GPA;
+
+	Education():GPA(0.0){}
+
+	Education(const string& school , double gpa)
+		:School(school),GPA(gpa)
+	{
+	}
+};
+
+struct Person
+{
+	string	Name;
+	int			Age;
+	vector<Education> Educations;
+};
+
+AJSON(Education,(School)(GPA))
+AJSON(Person,(Name)(Age)(Educations))
+
+int main(int argc, char * argv[])
+{
+	Person person;
+	person.Name = "Bob";
+	person.Age = 28;
+	person.Educations.push_back(Education("MIT",600));
+	string json_buff;
+	save_to_buff(person,json_buff);
+	std::cout << json_buff << std::endl;
+	cin.get();
+	return 0;
+}
