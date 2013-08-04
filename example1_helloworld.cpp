389158c54c76765ef80d483d0e4610cdcb523d3e
lordoffox <lordoffox@gmail.com>
2013-08-04 11:48:50 +0800

    Create example1_helloworld.cpp

---
+++ example1_helloworld.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+#include "ajson.hpp"
+
+using namespace std;
+using namespace boost::ajson;
+
+struct demo
+{
+  string hello;
+	string world;
+};
+
+AJSON(demo,(hello)(world))
+
+int main(int argc, char * argv[])
+{
+	char * buff = "{\"hello\" : \"Hello\", \"world\" : \"world.\"}";
+	demo the_demo;
+	string error_msg;
+	load_from_buff(the_demo,buff,error_msg);
+	cout << the_demo.hello << " " << the_demo.world << std::endl;
+	cin.get();
+	return 0;
+}
