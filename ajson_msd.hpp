1052b76deaf1874da8628c5e60e6a666ad4c9d0c
Evan <lordoffox@gmail.com>
2018-11-30 11:45:52 +0800

    add miss header file

--- ajson_msd.hpp
+++ ajson_msd.hpp
@@ -17,6 +17,7 @@ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 */
 
+#include <array>
 #include <cstdint>
 #include <cstring>
 #include <cstdio>
